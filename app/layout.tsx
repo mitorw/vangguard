93432d579399ad6bd36acacfac13fb050df38ab8
mitorw <mitorahmandhanawijaya@gmail.com>
2025-01-06 19:41:49 +0700

    mito : menambah login

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,12 +34,14 @@ export default function RootLayout({
                 
               </li>
             </ul>
+            <Link href="login">
+                  <button className="bg-white text-blue-500 px-4 py-2 rounded-md hover:bg-gray-200">
+                    Login
+                  </button>
+                </Link>
           </div>
         </nav>
 
-        <nav >
-          ini dashboard
-        </nav>
 
         {/* Main Content */}
         <section>{children}</section>
---
+++ app/login/page.tsx
@@ -0,0 +1,47 @@
+
+
+export default function loginPage() {
+    return (
+      <div className="flex items-center justify-center w-auto h-auto p-10 bg-gray-100">
+        <div className="bg-white p-8 rounded-md shadow-lg w-full max-w-md">
+          <h2 className="text-2xl font-bold mb-4 text-white text-center">Login</h2>
+          <form>
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded shadow-sm focus:ring-blue-500 focus:border-blue-500"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded shadow-sm focus:ring-blue-500 focus:border-blue-500"
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-500 text-white px-3 py-2 rounded hover:bg-blue-600"
+            >
+              Sign In
+            </button>
+          </form>
+          <p className="mt-4 text-sm text-gray-600 text-center">
+            Belum punya akun ?  <a href="signup" className = "text-blue-500 hover:underline">Sign Up</a>
+          </p>
+        </div>
+      </div>
+    );
+  }
+  
